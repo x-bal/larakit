b0734abb51fe2c9142c302e1c6c6961ae5cacb9a
mxbal <mxbal026@gmail.com>
2022-10-11 06:36:32 +0700

    Starter Template Laravel & AdminKit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,43 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="/dashboard">
+            <span class="align-middle">Larakit</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="/dashboard">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-profile.html">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-in.html">
+                    <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-up.html">
+                    <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item ">
+                <a class="sidebar-link" href="pages-blank.html">
+                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
